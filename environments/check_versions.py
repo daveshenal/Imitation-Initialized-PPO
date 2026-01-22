27291f933ea29a6eef482fa89c79c56fc24bf7be
Dave Shenal <daveshenal281@gmail.com>
2026-01-22 12:56:53 +0530

    add environment dir and rename report notebook

---
+++ environments/check_versions.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+import torch
+import cv2
+import numpy as np
+import ultralytics
+# import mediapipe as mp
+# import librosa
+# import joblib
+# import sounddevice as sd
+
+print("TensorFlow version:", tf.__version__)
+print("Torch version:", torch.__version__)
+print("OpenCV version:", cv2.__version__)
+print("NumPy version:", np.__version__)
+print("Ultralytics version:", ultralytics.__version__)
+# print("MediaPipe version:", mp.__version__)
+# print("Librosa version:", librosa.__version__)
+# print("Joblib version:", joblib.__version__)
+# print("Sounddevice version:", sd.__version__)
